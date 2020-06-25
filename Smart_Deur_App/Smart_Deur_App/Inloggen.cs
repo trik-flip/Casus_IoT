36743f0c2bc541c72cb79c3f54d467edc1bcf5e5
Shirley Aufdenkamp <1742965aufdenkamp@zuyd.nl>
2020-06-25 14:30:10 +0200

    forms

---
+++ Smart_Deur_App/Smart_Deur_App/Inloggen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Smart_Deur_App
+{
+    public partial class Inloggen : Form
+    {
+        public Inloggen()
+        {
+            InitializeComponent();
+        }
+
+        private void Inloggen_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
